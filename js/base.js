fcca0110d5435943e9a2d872ea0231f50c73c5e4
dengww <122011504@qq.com>
2018-06-13 16:47:01 +0800

    ++ 片区申请

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 //var apiHost = "http://api.9n.jxyunge.com";
 //var apiHost = "http://api.jx9n.com";
 var appVersion = "v3.0";  //app当前版本
-var apiHost = "http://192.168.1.109:8181";
+var apiHost = "http://192.168.1.115:8181";
 
 
 var API_URL = {};
