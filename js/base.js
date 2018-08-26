dba557d33c0ffd0fd0879841ee11e81ac30b3dd0
bianmaren <441889070@qq.com>
2018-08-27 06:17:57 +0800

    切换正式api

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 // var apiHost = "http://api.9n.jxyunge.com";
 var apiHost = "http://api.jx9n.com";
-var appVersion = "v3.0";  //app当前版本
+var appVersion = "v3.2";  //app当前版本
 //var apiHost = "http://192.168.31.176:8181";
 
 
