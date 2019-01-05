c5bf44a3a02de7350e024f595ddb401f61e10088
luhuiyue <lhywork@aliyun.com>
2019-01-05 17:11:02 +0800

    1111

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 var apiHost = "http://api.jx9n.com";
-var appVersion = "v3.3";
-apiHost = "http://39.106.62.16:8181";
+var appVersion = "v3.4";
+//apiHost = "http://39.106.62.16:8181";
 //apiHost = "http://192.168.1.17:8181";
 
 
